831838a7e9003bc4ddbf37bf0fa60417aa776232
jowu58 <103350997+jowu58@users.noreply.github.com>
2023-03-26 17:13:48 -0700

    fix error

--- app/src/main/java/com/example/monikers/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/monikers/MyBroadcastReceiver.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
-    private static final String TAG = "MyBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
